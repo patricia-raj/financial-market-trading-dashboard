d9d0ebc968acdf5ea18a0a3988c212a1b88ff990
bkamalnivas <bkamalnivas@gmail.com>
2022-08-03 20:08:50 -0400

    Rearrange python files

--- pages/Page_2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
--- pages/Page_3.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
