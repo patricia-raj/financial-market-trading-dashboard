97b3cc7f19666837eb56ffbacdde235ff1de63ce
“Patricia-r” <99471693+patricia-raj@users.noreply.github.com>
2022-08-04 05:50:10 -0500

    Visuals and presentable ticker financial information using yfinance

--- pages/3Stock_Financials.py
+++ pages/3Stock_Financials.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import Tickers 
+import altair as alt
 
 st.set_page_config(layout='wide')
 st.title('Real-Time - Stock Information' )
@@ -12,16 +13,6 @@ dropdown = st.selectbox('Pick vour assets', tickers)
 if len (dropdown) > 0:
     stock = yf.Ticker(dropdown)
     
-    # CSS to inject contained in a string
-    hide_dataframe_row_index = """
-            <style>
-            .row_heading.level0 {display:none}
-            .blank {display:none}
-            </style>
-            """
-    # Inject CSS with Markdown
-    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-
     # Display an interactive table
     
     #st.header('Stock Information of {}'.format(dropdown))
@@ -32,19 +23,45 @@ if len (dropdown) > 0:
     #st.dataframe(stock_info)
     #stockcalendar = stock.calendar
     #st.write(stockcalendar)
-    st.header('Financials of {}'.format(dropdown))
-    stockfin = stock.financials
-    st.write(stockfin)
-    st.header('Earnings of {}'.format(dropdown))
     stockearn = stock.earnings
-    st.write(stockearn)
-    st.header('Recommendations of {}'.format(dropdown))
+    earn_df = pd.DataFrame(stockearn)
+    with st.container():
+        st.header('Earnings of {}'.format(dropdown))
+        # You can call any Streamlit command, including custom components:
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.bar_chart(earn_df,width=1000, height=500, use_container_width=True)
+        with col2:
+            st.write(stockearn)
+    
+    st.header('Stock Price High and Low - {}'.format(dropdown))
+    price = [stock.info['dayHigh'], stock.info['dayLow'], stock.info['fiftyTwoWeekHigh'], stock.info['fiftyTwoWeekLow']]
+    index = ["Day High", "Day Low", "Year High", "Year Low"]
+    phl = pd.DataFrame({"HighLow": index, "Price": price})
+    phl.set_index('HighLow')
+    phl = phl.astype(str)
+    st.dataframe(phl)
+    
+    st.header('Financials - {}'.format(dropdown))
+    stockfin = stock.financials
+    fin_df = pd.DataFrame(stockfin)
+    fin_df.dropna(inplace=True)
+    fin_df = fin_df.transpose()
+    fin_df.index = pd.to_datetime(fin_df.index, format = '%Y-%m/%d')
+    fin_df.index = fin_df.index.strftime('%Y')
+    st.dataframe(fin_df)
+    
+    
+    st.header('Recommendations - {}'.format(dropdown))
     stockreco = stock.recommendations
-    st.write(stockreco)
-    st.header('Major Holders of {}'.format(dropdown))
+    st.dataframe(stockreco)
+    
+    st.header('Major Holders - {}'.format(dropdown))
     stockholders = stock.major_holders
-    st.write(stockholders)
-    st.header('Divident Information of {}'.format(dropdown))
+    st.dataframe(stockholders)
+    
+    st.header('Divident Information - {}'.format(dropdown))
     stockdiv = stock.dividends
-    st.write(stockdiv)
+    st.dataframe(stockdiv)
     
